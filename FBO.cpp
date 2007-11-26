3ed66b8cfeb0cb3d8645d3494651b9be5844b527
cybertron <cybertron@e6982656-166e-4d30-bf09-c8d9f77b066c>
2007-11-26 02:18:46 +0000

    Removing minor debugging.
    git-svn-id: http://www.nemebean.com/svn/coldest@21 e6982656-166e-4d30-bf09-c8d9f77b066c

--- FBO.cpp
+++ FBO.cpp
@@ -57,7 +57,7 @@ FBO& FBO::operator=(const FBO &f)
 void FBO::init()
 {
    glGenFramebuffersEXT(1, &fboid);
-   cout << "Creating framebuffer object " << fboid << endl;
+   //cout << "Creating framebuffer object " << fboid << endl;
    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fboid);
    
    glGenTextures(2, tex);
