40f8a09a3c2c486a591d0369120405bb2ce74c58
cybertron <cybertron@e6982656-166e-4d30-bf09-c8d9f77b066c>
2008-01-27 23:27:13 +0000

    Moving things around
    git-svn-id: http://www.nemebean.com/svn/coldest@89 e6982656-166e-4d30-bf09-c8d9f77b066c

---
+++ gui/ProgressBar.cpp
@@ -0,0 +1,59 @@
+#include "ProgressBar.h"
+
+ProgressBar::ProgressBar(GUI* p, TextureManager* tm)
+{
+   value = 0;
+   minvalue = 0;
+   maxvalue = 100;
+   Init(p, tm);
+}
+
+
+ProgressBar::~ProgressBar()
+{
+   //Cleanup();
+}
+
+
+void ProgressBar::SetRange(int min, int max)
+{
+   minvalue = min;
+   maxvalue = max;
+}
+
+
+void ProgressBar::Render()
+{
+   if (!visible) return;
+   RenderBase();
+   
+   float position = (float)(value - minvalue) / (float)(maxvalue - minvalue) * width;
+   texman->BindTexture(textures[Hover]);
+   glBegin(GL_TRIANGLE_STRIP);
+   glTexCoord2i(0, 0);
+   glVertex2f((x + xoff) * wratio, (y + yoff) * hratio);
+   glTexCoord2i(0, 1);
+   glVertex2f((x + xoff) * wratio, (y + yoff + height) * hratio);
+   glTexCoord2i(1, 0);
+   glVertex2f((x + xoff + position) * wratio, (y + yoff) * hratio);
+   glTexCoord2i(1, 1);
+   glVertex2f((x + xoff + position) * wratio, (y + yoff + height) * hratio);
+   glEnd();
+}
+
+
+void ProgressBar::ReadNode(DOMNode* current, GUI* parent)
+{
+   /* This is not the most efficient way to handle tags, but it's the least troublesome and
+      I somehow doubt that XML parsing performance is going to be a gating factor for us.*/
+   InitTags();
+   short type = current->getNodeType();
+   if (type)
+   {
+      if (type == DOMNode::ELEMENT_NODE)
+      {
+         ReadTextures(current);
+      }
+   }
+   DestroyTags();
+}
---
+++ gui/TextArea.cpp
@@ -0,0 +1,134 @@
+#include "TextArea.h"
+
+TextArea::TextArea(GUI* p, TextureManager* tm)
+{
+   Init(p, tm);
+   table = new Table(this, texman);
+   table->rowheight = 20;
+   table->colwidths = ToString(width);
+   table->x = 0;
+   table->y = 0;
+   maxlines = 100;
+}
+
+
+TextArea::~TextArea()
+{
+   delete table;
+}
+
+
+void TextArea::ReadNode(DOMNode *current, GUI* parentw)
+{
+   short type = current->getNodeType();
+   if (type)
+   {
+      if (type == DOMNode::ELEMENT_NODE)
+      {
+         InitTags();
+         
+         ReadTextures(current);
+         
+         string curr;
+         curr = ReadStringTag(current, tag.leftclickaction);
+         if (curr != "")
+         {
+            leftclickaction = curr;
+         }
+         
+         DestroyTags();
+      }
+      else if (type == DOMNode::TEXT_NODE)
+      {
+         char *currstr;
+         currstr = XMLString::transcode(current->getNodeValue());
+         if (string(currstr).find_first_not_of(" \n") != string::npos)
+            Append(currstr);
+         XMLString::release(&currstr);
+      }
+   }
+}
+
+
+void TextArea::Render()
+{
+   if (!visible) return;
+   xoff = parent->xoff + parent->x;
+   yoff = parent->yoff + parent->y;
+   
+   RenderBase();
+   
+   table->width = width;
+   table->height = height;
+   table->Render();
+}
+
+
+void TextArea::ProcessEvent(SDL_Event* event)
+{
+   if (!visible) return;
+   switch (event->type)
+   {
+      case SDL_MOUSEBUTTONDOWN:
+         if (InWidget(event->motion.x, event->motion.y))
+            table->ProcessEvent(event);
+         break;
+      
+      default:
+         table->ProcessEvent(event);
+         break;
+   }
+}
+
+
+void TextArea::Append(string s)
+{
+   text += s;
+   Refresh();
+}
+
+
+void TextArea::Refresh()
+{
+   string currstring = "";
+   string working = text;
+   int swidth, sheight;
+   int lastbreak = 0;
+   bool foundbreak = false;
+   bool newline = false;
+   table->clear();
+   for (int i = 1; i < working.length(); ++i)
+   {
+      if (working[i] == ' ' || working[i] == '\n')
+      {
+         lastbreak = i;
+         foundbreak = true;
+         if (working[i] == '\n')
+         {
+            newline = true;
+            foundbreak = false;
+         }
+      }
+      
+      StringDim(font, working.substr(0, i), swidth, sheight);
+      float scale = (table->rowheight - ymargin * 2.f) * hratio / (float)sheight;
+      float fw = (float)swidth / wratio * scale;
+      
+      if ((fw > width - xmargin * 2.f) || newline)
+      {
+         if (!foundbreak)
+            lastbreak = i;
+         
+         table->Add(working.substr(0, lastbreak));
+         
+         // If we didn't actually find a space then don't skip a character
+         if (!foundbreak && !newline)
+            --lastbreak;
+         working = working.substr(lastbreak + 1);
+         foundbreak = false;
+         newline = false;
+         lastbreak = 0;
+         i = 1;
+      }
+   }
+}
