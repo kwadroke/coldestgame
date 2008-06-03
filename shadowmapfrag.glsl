26519e90668b474970f09922387db83b95ea1c0e
Ben Nemec <cybertron@nemebean.com>
2008-06-03 04:39:56 +0000

    VSM changes to (hopefully) make it work better on 16 bit textures

--- shadowmapfrag.glsl
+++ shadowmapfrag.glsl
@@ -8,8 +8,13 @@ void main()
    vec4 color = texture2D(tex, gl_TexCoord[0].st);
    
    // Write VSM values
-   vec2 m = vec2(dist, dist * dist); // Not sure which we want to use yet
-   //m = vec2(gl_FragCoord.z, gl_FragCoord.z * gl_FragCoord.z);
-   
-   gl_FragColor = vec4(m, 0, color.a);
+   float c = 5.;
+   float d = dist * 2. - 1;
+   float adjdist = exp(exp(c * d));
+   vec2 m = vec2(adjdist, adjdist * adjdist);
+   // Note: this should be -exp(exp(-c * d)), but that messes up our floating point precision, so we add the - in the receiving shader
+   adjdist = exp(exp(-c * d));
+   vec2 mneg = vec2(adjdist, adjdist * adjdist);
+   mneg.y *= step(.5, color.a); // Simulate alpha test
+   gl_FragColor = vec4(m, mneg);
 }
