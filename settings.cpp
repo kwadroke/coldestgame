4ec20df749e948f1b18beb5ed2b9bb07eca1744e
Ben Nemec <cybertron@nemebean.com>
2008-08-22 05:07:22 +0000

    Add shadows and reflection to settings screen

--- settings.cpp
+++ settings.cpp
@@ -9,6 +9,8 @@ void UpdateSettings()
    Slider* grassdensityslider = dynamic_cast<Slider*>(gui[settings]->GetWidget("grassdensityslider"));
    Slider* grassdistslider = dynamic_cast<Slider*>(gui[settings]->GetWidget("grassdistslider"));
    Slider* impdistslider = dynamic_cast<Slider*>(gui[settings]->GetWidget("impdistslider"));
+   Button* shadowsbutton = dynamic_cast<Button*>(gui[settings]->GetWidget("shadowsbutton"));
+   Button* reflectionbutton = dynamic_cast<Button*>(gui[settings]->GetWidget("reflectionbutton"));
    
    partupdintslider->value = console.GetInt("partupdint");
    partcountslider->value = 0;//console.GetInt("partcount");
@@ -16,6 +18,8 @@ void UpdateSettings()
    grassdensityslider->value = console.GetInt("grassdensity");
    grassdistslider->value = console.GetInt("grassdist");
    impdistslider->value = console.GetInt("impdistmulti");
+   shadowsbutton->togglestate = console.GetBool("shadows") ? 1 : 0;
+   reflectionbutton->togglestate = console.GetBool("reflection") ? 1 : 0;
 }
 
 
@@ -27,6 +31,8 @@ void SaveSettings()
    Slider* grassdensityslider = dynamic_cast<Slider*>(gui[settings]->GetWidget("grassdensityslider"));
    Slider* grassdistslider = dynamic_cast<Slider*>(gui[settings]->GetWidget("grassdistslider"));
    Slider* impdistslider = dynamic_cast<Slider*>(gui[settings]->GetWidget("impdistslider"));
+   Button* shadowsbutton = dynamic_cast<Button*>(gui[settings]->GetWidget("shadowsbutton"));
+   Button* reflectionbutton = dynamic_cast<Button*>(gui[settings]->GetWidget("reflectionbutton"));
    
    console.Parse("setsave partupdint " + ToString(partupdintslider->value), false);
    // Not implemented yet
@@ -35,4 +41,6 @@ void SaveSettings()
    console.Parse("setsave grassdensity " + ToString(float(grassdensityslider->value) / 100.f), false);
    console.Parse("setsave grassdist " + ToString(grassdistslider->value), false);
    console.Parse("setsave impdistmulti " + ToString(impdistslider->value), false);
+   console.Parse("setsave shadows " + ToString(shadowsbutton->togglestate), false);
+   console.Parse("setsave reflection " + ToString(reflectionbutton->togglestate), false);
 }
