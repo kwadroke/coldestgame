194e2ed8c871aabdc3e088e7cca9253e269ec1ed
cybertron <cybertron@e6982656-166e-4d30-bf09-c8d9f77b066c>
2009-03-23 00:46:34 +0000

    Fix bugs with keymapping, apply GPL to main C++ source files
    git-svn-id: http://www.nemebean.com/svn/coldest@448 e6982656-166e-4d30-bf09-c8d9f77b066c

--- IDGen.cpp
+++ IDGen.cpp
@@ -1,3 +1,23 @@
+// @Begin License@
+/***********************************************************************
+   This file is part of Coldest.
+
+   Coldest is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Coldest is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Coldest.  If not, see <http://www.gnu.org/licenses/>.
+   
+   Copyright 2008, 2009 Ben Nemec
+***********************************************************************/
+// @End License@
 #include "IDGen.h"
 
 IDGen::IDGen() : nextid(0), incmutex(SDL_CreateMutex())
