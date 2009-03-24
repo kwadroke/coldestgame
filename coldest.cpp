4719c15b4199c9adc4191fbf240eb9d8d8d0c150
cybertron <cybertron@e6982656-166e-4d30-bf09-c8d9f77b066c>
2009-03-24 04:42:55 +0000

    Apply license to shaders as well
    git-svn-id: http://www.nemebean.com/svn/coldest@449 e6982656-166e-4d30-bf09-c8d9f77b066c

--- coldest.cpp
+++ coldest.cpp
@@ -1,23 +1,22 @@
 // @Begin License@
-/***********************************************************************
-   This file is part of Coldest.
-
-   Coldest is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Coldest is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Coldest.  If not, see <http://www.gnu.org/licenses/>.
-   
-   Copyright 2008, 2009 Ben Nemec
-***********************************************************************/
+// This file is part of Coldest.
+//
+// Coldest is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Coldest is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Coldest.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2008, 2009 Ben Nemec
 // @End License@
+
 // Main loop and basic game logic - a rather large file that could probably afford to be
 // split up somewhat, but it hasn't really been a problem thus far.
 
@@ -366,7 +365,6 @@ void ReadConfig()
    keys.keyleft = SDLKey(console.GetInt("keyleft"));
    keys.keyright = SDLKey(console.GetInt("keyright"));
    keys.keyloadout = SDLKey(console.GetInt("keyloadout"));
-   logout << console.GetInt("mousefire") << endl;
    keys.mousefire = console.GetInt("mousefire");
    keys.mousezoom = console.GetInt("mousezoom");
    keys.mouseuse = console.GetInt("mouseuse");
