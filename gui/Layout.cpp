2bac772d3f9226d0c73ad4a43def7a81e3a9a30e
Ben Nemec <cybertron@nemebean.com>
2010-03-04 03:57:16 +0000

    Update copyright and apply license boilerplate to new files

--- gui/Layout.cpp
+++ gui/Layout.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Coldest.  If not, see <http://www.gnu.org/licenses/>.
 //
-// Copyright 2008, 2009 Ben Nemec
+// Copyright 2008, 2010 Ben Nemec
 // @End License@
 
 #include "Layout.h"
--- shaders/foliagefrag.glsl
+++ shaders/foliagefrag.glsl
@@ -1,3 +1,21 @@
+// @Begin License@
+// This file is part of Coldest.
+//
+// Coldest is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Coldest is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Coldest.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright 2008, 2010 Ben Nemec
+// @End License@
 void basiclighting(inout vec3, in vec3, out vec4, out vec4, out vec4, in float);
 void shadow(vec4, vec4, float, inout vec4);
 void fog(float, inout vec4);
