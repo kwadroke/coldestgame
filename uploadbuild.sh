d38d0732ddf86728dbe2fe07ff7077a964ab3d56
Ben Nemec <cybertron@nemebean.com>
2010-05-12 03:00:36 +0000

    Use rsync for uploadbuild.sh Upload full build tarballs as well

--- uploadbuild.sh
+++ uploadbuild.sh
@@ -4,9 +4,9 @@ REVISION=`svn info | grep Revision | cut -f 2 -d \ `
 
 arch=`uname -m`
 
-ssh coldestgame.com "rm -rf /var/www/files/updates/linux/$arch/*"
+rsync -avz -e ssh Coldest$REVISION/* coldestgame.com:/var/www/files/updates/linux/$arch
 
-scp -r Coldest$REVISION/* coldestgame.com:/var/www/files/updates/linux/$arch
+rsync -avz -e ssh Coldest$REVISION*.tar.gz coldestgame.com:/var/www/files
 
 ssh coldestgame.com "cd /var/www/files/updates/linux/$arch && /usr/local/sbin/gencrcfile"
 
