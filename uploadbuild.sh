d77e1e533aab5b39e3b6a30fd53d232c5ca3703e
cybertron <cybertron@e6982656-166e-4d30-bf09-c8d9f77b066c>
2010-05-12 03:07:58 +0000

    Fix wrong extension in upload script
    git-svn-id: http://www.nemebean.com/svn/coldest@604 e6982656-166e-4d30-bf09-c8d9f77b066c

--- uploadbuild.sh
+++ uploadbuild.sh
@@ -6,7 +6,7 @@ arch=`uname -m`
 
 rsync -avz -e ssh Coldest$REVISION/* coldestgame.com:/var/www/files/updates/linux/$arch
 
-rsync -avz -e ssh Coldest$REVISION*.tar.gz coldestgame.com:/var/www/files
+rsync -avz -e ssh Coldest$REVISION*.tar.bz2 coldestgame.com:/var/www/files
 
 ssh coldestgame.com "cd /var/www/files/updates/linux/$arch && /usr/local/sbin/gencrcfile"
 
