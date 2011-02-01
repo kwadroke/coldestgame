dc3c2ba2e903a4e206afd67757438e9254ba7a56
cybertron <cybertron@e6982656-166e-4d30-bf09-c8d9f77b066c>
2011-02-01 04:52:35 +0000

    Mutex debugging Change bush leaves to match new terrain textures better
    git-svn-id: http://www.nemebean.com/svn/coldest@667 e6982656-166e-4d30-bf09-c8d9f77b066c

--- Mutex.h
+++ Mutex.h
@@ -5,6 +5,7 @@
 #include <SDL/SDL_thread.h>
 #include <boost/shared_ptr.hpp>
 #include "Timer.h"
+#include "util.h"
 
 
 class Mutex
@@ -20,25 +21,33 @@ class Mutex
 
    private:
       SDL_mutex *mutex;
+      Timer t;
 };
 
 typedef boost::shared_ptr<Mutex> MutexPtr;
 
 inline int Mutex::lock()
 {
-   Timer t;
-   t.start();
+   Timer timer;
+   timer.start();
    int retval = SDL_mutexP(mutex);
-   if (t.elapsed() > 250)
-      logout << "Long mutex lock " << t.elapsed() << endl;
+   if (timer.elapsed() > 250)
+   {
+      logout << "Long lock wait " << gettid() << " for " << timer.elapsed() << " ms" << endl;
+   }
+   t.start();
    return retval;
 }
 
 
 inline int Mutex::unlock()
 {
+   if (t.elapsed() > 250)
+   {
+      logout << "Long held lock " << gettid() << " for " << t.elapsed() << " ms" << endl;
+   }
    int retval = SDL_mutexV(mutex);
-   SDL_Delay(0); // Prevent thread starvation
+   //SDL_Delay(0); // Prevent thread starvation
    return retval;
 }
 
