b94c9e63fe6694650253611d64a52bd000c8c82c
Ben Nemec <cybertron@nemebean.com>
2012-01-21 13:17:15 -0600

    2012 license update

--- Mesh.cpp
+++ Mesh.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Coldest.  If not, see <http://www.gnu.org/licenses/>.
 //
-// Copyright 2008, 2011 Ben Nemec
+// Copyright 2008-2012 Ben Nemec
 // @End License@
 #include "Mesh.h"
 #include "util.h"
