b5c309016d07c50f71ecc3b282e179f73e0089d0
Ben Nemec <cybertron@nemebean.com>
2012-02-23 22:41:39 -0600

    Remove fog from flat shader

--- shaders/flatfrag.glsl
+++ shaders/flatfrag.glsl
@@ -28,14 +28,10 @@ varying vec3 worldcoords;
 
 void main()
 {
-   /* Reflection */
-   //if (worldcoords.y < 0. && reflectval > .5) discard;
-   
    /* Texturing */
-   
    vec4 color = texture2D(tex, gl_TexCoord[0].st) * gl_Color;
    
-   fog(dist, color);
+   //fog(dist, color);
    
    gl_FragColor = color;
 }
